60ec69febfa72cd909d415e8f0e0f63a87b7bf7a
MazeQhi404 <maze.putuma@gmail.com>
2025-03-16 13:35:56 +0200

    attached a .catch incase any errors get thrown and wrote a function that sets a default background image in case of these errors

--- index.js
+++ index.js
@@ -3,14 +3,21 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
       .then(res => res.json()) // changes from json to javascript 
       .then(data => { 
+            console.log(data.urls.regular)
             document.body.style.backgroundImage = `url(${data.urls.regular})`
             document.getElementById('author').textContent = ` By: ${data.user.name$}` // displays the authors name
 
       })
-      .catch(err => {
+
+      //.then blocks are written assuming that every promise will be resoled correctly 
+      .catch(err => { //.catch runs everytime an error gets thrown
             
             console.log("Error");
-            document.body.style.backgroundImage = "url('image.jpg')"
+            //document.body.style.backgroundImage = `url("this is where the defuault url should go")`
+            const img = document.querySelector("img");
+            img.src = "https://www.wordforest.org/2016/12/16/glossary-forestry-terms/"
+
+            
             
       }); 
 
